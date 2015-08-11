380efb9cdc3b541a4df635458e87817ea1c985cd
Daniel Buchan <clone@myself.com>
2015-08-11 11:34:30 +0100

    species_count now works

---
+++ hash_submission_lookup/species_count.py
@@ -0,0 +1,31 @@
+import pprint
+import requests
+import re
+
+id_re = ">UniRef100_(.+?)\s"
+count_re = "COUNT=(.+)"
+taxa_re = "OX\s+NCBI_TaxID=(\d+)\D+"
+uniprot_uri = "http://www.uniprot.org/uniprot/"  # A0A024RDF4.txt
+
+totals = {}
+
+with open('hash_matches.txt') as infile:
+    for l in infile:
+        seq_id = ''
+        count = 0
+        for match in re.findall(id_re, l):
+            seq_id = match
+            for match2 in re.findall(count_re, l):
+                count = int(match2)
+                try:
+                    r = requests.post(uniprot_uri+seq_id+".txt")
+                except:
+                    print("Missed " + seq_id)
+                for match3 in re.findall(taxa_re, r.text):
+                    if match3 in totals:
+                        totals[match3] += match2
+                    else:
+                        totals[match3] = match2
+
+for taxa_id in totals:
+    print(taxa_id+","+totals[taxa_id])
