0eba26b0d93129df02e9f3216dcb827c67c516d9
Daniel Buchan <clone@myself.com>
2015-08-17 13:16:18 +0100

    updated to make sge script more thread/process safe

--- sge_scripts/run_blast.sh
+++ sge_scripts/run_blast.sh
@@ -4,18 +4,29 @@
 #
 # Test we have uniref90 available and if not copy it to /tmp
 #
-blastdb="/tmp/test_db.fasta"
+#
+# LOCAL CONFIG
+#
+TMP="tmp/psi_cache"
+FASTA_PROTEOMES="/Users/dbuchan/Code/psipred_cache/get_proteomes/cache_proteomes_single_lines.fasta"
+BLAST_EXE="/Users/dbuchan/Downloads/ncbi-blast-2.2.31+/bin/psiblast"
+FINAL="./"
+blastdb_name="test_db"
+blastdb_location="/Users/dbuchan/Downloads/"
+blastdb="/$TMP/$blastdb_name.fasta"
+SGE_TASK_ID=1
+
+#Check if we have a copy of the db and mv it if neccessary
 if [ -f "$blastdb" ]
 then
   echo "Blast DB present"
 else
   echo "Copying blast db"
-  cp /Users/dbuchan/Downloads/test_db.* /tmp/
+  mkdir /$TMP
+  cp $blastdb_location/$blastdb_name.* /$TMP
 fi
 
-#Here we get the
-FASTA_PROTEOMES="/Users/dbuchan/Code/psipred_cache/get_proteomes/cache_proteomes_single_lines.fasta"
-SGE_TASK_ID=1
+#Here we get the sequence
 SEQ_LOC=$(expr $SGE_TASK_ID '*' '2')
 HEADER_LOC=$(expr $SEQ_LOC '-' '1')
 HEADER=$(awk "NR==$HEADER_LOC  {print;exit}" $FASTA_PROTEOMES)
@@ -23,20 +34,20 @@ SEQ=$(awk "NR==$SEQ_LOC  {print;exit}" $FASTA_PROTEOMES)
 
 #write HEADER AND SEQ TO A /tmp/temp file
 MATCH=$(echo $HEADER | perl -ne 'while(/>.+\|(.+?)\|.+\s/g){print "$1\n";}')
-FILENAME="/tmp/$MATCH.fasta"
-OUT="/tmp/$MATCH.bls"
-PSSM="/tmp/$MATCH.pssm"
+FILENAME="/$TMP/$MATCH.fasta"
+OUT="/$TMP/$MATCH.bls"
+PSSM="/$TMP/$MATCH.pssm"
 CONTENTS="$HEADER\n$SEQ"
 echo $CONTENTS >> $FILENAME
 
 #run blast
 echo "RUNNING A BLAST"
-/Users/dbuchan/Downloads/ncbi-blast-2.2.31+/bin/psiblast -query $FILENAME -out_pssm $PSSM -out $OUT -db $blastdb -num_iterations 20
+$BLAST_EXE -query $FILENAME -out_pssm $PSSM -out $OUT -db $blastdb -num_iterations 20
 FAILFLAG="./$SGE_TASK_ID.failure"
 if [ -f "$PSSM" ]
 then
-  mv $PSSM ./
-  mv $OUT ./
+  mv $PSSM $FINAL
+  mv $OUT $FINAL
   rm $FILENAME
 else
   touch $FAILFLAG
