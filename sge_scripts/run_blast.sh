b93cbe8a622d80cd1b0e48068c95deae216f6746
Daniel Buchan <clone@myself.com>
2015-08-20 13:53:29 +0100

    minor lock file changes

--- sge_scripts/run_blast.sh
+++ sge_scripts/run_blast.sh
@@ -3,8 +3,8 @@
 #$ -t 1-409314
 #$ -l h_rt=2:0:0
 #$ -l tmem=1.9G -l h_vmem=1.9G
-#$ -e /home/dbuchan/psipred_cache/error.txt
-#$ -o /home/dbuchan/psipred_cache/output.txt
+#$ -e /home/dbuchan/psipred_cache/error/$TASK_ID.err
+#$ -o /home/dbuchan/psipred_cache/output/$TASK_ID.out
 
 # for i in `qhost | cut -f 1 -d " " ` ; do ssh  -oBatchMode=yes $i "echo $i; rm -rf /scratch0/dbuchan" ; done
 # once we're done we can use this to clean up
@@ -21,13 +21,10 @@
 # blastdb_location="/Users/dbuchan/Downloads/"
 # blastdb="/$TMP/$blastdb_name.fasta"
 # SGE_TASK_ID=1
-while [ $TMP/lock ]
-do
-  sleep 5
-done
-
 # This config for morecambe/sge
+hostname
 TMP="/scratch0/dbuchan/psi_cache"
+LOCK="$TMP/lock"
 FASTA_PROTEOMES="/home/dbuchan/psipred_cache/cache_proteomes_single_lines.fasta"
 BLAST_EXE="/home/dbuchan/ncbi-blast-2.2.31+-src/c++/ReleaseMT/bin/psiblast"
 FINAL="/home/dbuchan/psipred_cache/batch_1/"
@@ -36,7 +33,11 @@ blastdb_location="/home/dbuchan/uniref/"
 blastdb="/$TMP/$blastdb_name.fasta"
 # SGE_TASK_ID=1
 
-
+while [ -f $LOCK ]
+do
+  echo "waiting"
+  sleep 10
+done
 #Check if we have a copy of the db and mv it if neccessary
 if [ -f "$blastdb" ]
 then
@@ -44,9 +45,9 @@ then
 else
   echo "Copying blast db"
   mkdir -p $TMP
-  touch $TMP/lock
+  touch $LOCK
   cp $blastdb_location/$blastdb_name.* /$TMP
-  rm $TMP/lock
+  rm $LOCK
   #I should check if the last files makes it
 fi
 
