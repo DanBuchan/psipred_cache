209c8b83153ebab6c7bd5669ab9c21eeefe8a0cf
Daniel Buchan <clone@myself.com>
2015-08-20 16:51:10 +0100

    removed the file copying, we'll do this as a commandline operation up front

--- sge_scripts/run_blast.sh
+++ sge_scripts/run_blast.sh
@@ -6,6 +6,9 @@
 #$ -e /home/dbuchan/psipred_cache/error/$TASK_ID.err
 #$ -o /home/dbuchan/psipred_cache/output/$TASK_ID.out
 
+# for i in `qhost | cut -f 1 -d " " ` ; do ssh  -oBatchMode=yes $i "echo $i; mkdir -p /scratch0/dbuchan/psi_cache; scp morecambe2:/home/dbuchan/psipred_cache/proteomes_greater_than_ten_percent_prepped.fasta /scratch0/dbuchan/psi_cache/" ; done
+# for i in `qhost | cut -f 1 -d " " ` ; do ssh  -oBatchMode=yes $i "echo $i; scp morecambe2:/home/dbuchan/uniref/uniref90.fasta.* /scratch0/dbuchan/psi_cache/" ; done
+
 # for i in `qhost | cut -f 1 -d " " ` ; do ssh  -oBatchMode=yes $i "echo $i; rm -rf /scratch0/dbuchan" ; done
 # once we're done we can use this to clean up
 
@@ -26,9 +29,10 @@ hostname
 TMP="/scratch0/dbuchan/psi_cache"
 LOCK="$TMP/lock"
 FASTA_PROTEOMES="/home/dbuchan/psipred_cache/cache_proteomes_single_lines.fasta"
+LOCAL_PROTEOMES="$TMP/cache_proteomes_single_lines.fasta"
 BLAST_EXE="/home/dbuchan/ncbi-blast-2.2.31+-src/c++/ReleaseMT/bin/psiblast"
 FINAL="/home/dbuchan/psipred_cache/batch_1/"
-blastdb_name="test_db"
+blastdb_name="uniref90"
 blastdb_location="/home/dbuchan/uniref/"
 blastdb="/$TMP/$blastdb_name.fasta"
 # SGE_TASK_ID=1
@@ -44,18 +48,19 @@ then
   echo "Blast DB present"
 else
   echo "Copying blast db"
-  mkdir -p $TMP
-  touch $LOCK
-  cp $blastdb_location/$blastdb_name.* /$TMP
-  rm $LOCK
+  # mkdir -p $TMP
+  # touch $LOCK
+  # cp $FASTA_PROTEOMES /$TMP
+  # cp $blastdb_location/$blastdb_name.* /$TMP
+  # rm $LOCK
   #I should check if the last files makes it
 fi
 
 #Here we get the sequence
 SEQ_LOC=$(expr $SGE_TASK_ID '*' '2')
 HEADER_LOC=$(expr $SEQ_LOC '-' '1')
-HEADER=$(awk "NR==$HEADER_LOC  {print;exit}" $FASTA_PROTEOMES)
-SEQ=$(awk "NR==$SEQ_LOC  {print;exit}" $FASTA_PROTEOMES)
+HEADER=$(awk "NR==$HEADER_LOC  {print;exit}" $LOCAL_PROTEOMES)
+SEQ=$(awk "NR==$SEQ_LOC  {print;exit}" $LOCAL_PROTEOMES)
 
 #write HEADER AND SEQ TO A /tmp/temp file
 MATCH=$(echo $HEADER | perl -ne 'while(/>.+\|(.+?)\|.+\s/g){print "$1\n";}')
